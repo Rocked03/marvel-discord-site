c6c3fc1dd7f0af1a122ee6ce3cd67cab78ea55fd
Samuel Ou <sou323@aucklanduni.ac.nz>
2025-03-22 18:35:23 +1300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import styled from "styled-components";
+
+const PageBody = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 24px;
+`;
+
+export default function Home() {
+  return <PageBody>Website under construction!</PageBody>;
+}
