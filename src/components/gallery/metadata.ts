84fa339ad1026108c76840e3473cfa7bb11a5e51
Rocked03 <rockedohthree@gmail.com>
2025-03-26 18:06:15 +1300

    fix: tsx to ts

---
+++ src/components/gallery/metadata.ts
@@ -0,0 +1,65 @@
+import type { GalleryEntry } from "@/types";
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { formatGalleryEntryTitle } from ".";
+import { defaultMetadata } from "@/lib/metadata";
+
+export interface GalleryPageProps {
+	searchParams: { slide?: string };
+}
+
+interface GalleryPageBaseProps extends GalleryPageProps {
+	galleryEntries: GalleryEntry[];
+}
+
+// Server-side Metadata Generation
+export async function generateMetadataBase({
+	searchParams,
+	galleryEntries,
+}: GalleryPageBaseProps): Promise<Metadata> {
+	if (galleryEntries.length > 0) {
+		return {
+			title: "aaaaaaMarvel Discord",
+			description:
+				"Taaaahe largest community-run server for everything Marvel.",
+			openGraph: {
+				title: "Maaaaaarvel Discord",
+				description:
+					"The aaaaaalargest community-run server for everything Marvel.",
+				siteName: "Marvaaaaaaaaael Discord",
+			},
+			icons: {
+				icon: [{ url: "/img/gallery/logo/Logo with full icon.png" }],
+			},
+		};
+	}
+
+	const slideTitle = searchParams.slide;
+
+	if (!slideTitle) {
+		return {
+			title: "Marvel Discord Gallery",
+			description: "Explore the gallery archive of the Marvel Discord.",
+		};
+	}
+
+	const entry = galleryEntries.find(
+		(entry) => formatGalleryEntryTitle(entry.title) === slideTitle,
+	);
+
+	if (!entry) return notFound(); // Handle case where slide doesn't exist
+
+	return {
+		...defaultMetadata,
+		title: `${entry.title} - Gallery`,
+		description: entry.description || "View this gallery entry.",
+		openGraph: {
+			...defaultMetadata.openGraph,
+			title: `${entry.title} - Gallery`,
+			description: entry.description || "View this gallery entry.",
+			siteName: "Marvel Discord - Gallery",
+			images:
+				entry.imageUrls.length > 0 ? [{ url: entry.imageUrls[0] }] : undefined,
+		},
+	};
+}
