a8013afef30031c8cc526c155ffaf150351bc3e1
Rocked03 <rockedohthree@gmail.com>
2025-03-26 18:08:47 +1300

    feat: more test

--- src/app/gallery/logo/metadata.ts
+++ src/app/gallery/logo/metadata.ts
@@ -1,12 +1,28 @@
 import {
-  type GalleryPageProps,
-  generateMetadataBase,
+	type GalleryPageProps,
+	generateMetadataBase,
 } from "@/components/gallery";
 import { galleryEntries } from "./entries";
 import type { Metadata } from "next";
 
 export async function generateMetadata({
-  searchParams,
+	searchParams,
 }: GalleryPageProps): Promise<Metadata> {
-  return await generateMetadataBase({ searchParams, galleryEntries });
+	if (galleryEntries.length > 0) {
+		return {
+			title: "aaaaaaMarvel Discord",
+			description:
+				"Taaaahe largest community-run server for everything Marvel.",
+			openGraph: {
+				title: "Maaaaaarvel Discord",
+				description:
+					"The aaaaaalargest community-run server for everything Marvel.",
+				siteName: "Marvaaaaaaaaael Discord",
+			},
+			icons: {
+				icon: [{ url: "/img/gallery/logo/Logo with full icon.png" }],
+			},
+		};
+	}
+	return await generateMetadataBase({ searchParams, galleryEntries });
 }
