2054ee83af69b62cb37e50337cda3f9d4848319e
Rocked03 <rockedohthree@gmail.com>
2025-06-07 21:27:37 +1200

    style: minor tweak

--- src/components/polls/fixedPositionContainer.tsx
+++ src/components/polls/fixedPositionContainer.tsx
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 export const FixedPositionContainer = styled.div`
   align-items: end;
-  bottom: 2rem;
+  bottom: 0;
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
   justify-content: right;
+  padding: 2rem;
   position: fixed;
-  right: 2rem;
+  right: 0;
 `;
