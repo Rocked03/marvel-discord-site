08e7f956adff5c4f4c3771d08c48ac8d2a452163
Rocked03 <rockedohthree@gmail.com>
2025-06-20 22:44:35 +1200

    feat: updated metadata (#14)

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -2,21 +2,21 @@ import { relativeImagePathToAbsolute } from "@/utils";
 import type { Metadata, Viewport } from "next";
 
 export const defaultMetadata: Metadata = {
-	title: "Marvel Discord",
-	description: "The largest community-run server for everything Marvel.",
-	openGraph: {
-		type: "website",
-		title: "Marvel Discord",
-		description: "The largest community-run server for everything Marvel.",
-		siteName: "Marvel Discord",
-		images: [
-			relativeImagePathToAbsolute(
-				"/img/gallery/banner/Banner centred text.png",
-			),
-		],
-	},
+  title: "Marvel Discord",
+  description: "The largest community-run server for everything Marvel.",
+  openGraph: {
+    type: "website",
+    title: "Marvel Discord",
+    description: "The largest community-run server for everything Marvel.",
+    siteName: "Marvel Discord",
+    images: [
+      relativeImagePathToAbsolute(
+        "/img/gallery/banner/Marvel Discord Banner.png"
+      ),
+    ],
+  },
 };
 
 export const defaultViewport: Viewport = {
-	themeColor: "#ea2328",
+  themeColor: "#ea2328",
 };
